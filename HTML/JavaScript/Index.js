4338fd4720c4cf1faad5be1339d6e2e87922fe3b
Uwem Ndah <ndahuwem@gmail.com>
2022-05-21 11:05:56 +0100

    hosting

---
+++ HTML/JavaScript/Index.js
@@ -0,0 +1,9 @@
+let firstName= prompt("what is your name")
+alert('Hello' + firstName + "!")
+let phrase =  ('I havent been to chile',the other day.');
+/*
+for comment that wll not display
+*/
+typeOf "2";
+typeOf true
+
